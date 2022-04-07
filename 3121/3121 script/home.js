a90f88f63ac3d54247d30d561e74f2717859ddae
find-3121-and-do-not-see-the-code <102374922+find-3121-and-do-not-see-the-code@users.noreply.github.com>
2022-04-07 18:01:29 +0000

    Add files via upload

---
+++ 3121/3121 script/home.js
@@ -0,0 +1,9 @@
+const burger=document.querySelector(".lol")
+const asj=document.getElementById("asj")
+
+burger.addEventListener('click',()=>
+{
+  burger.classList.toggle("li")
+  asj.classList.toggle("on")
+}
+)
